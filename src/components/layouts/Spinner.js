f52c413ab545b347d75ccb6f65373d12dcbbe186
aushtiin <aotangban@gmail.com>
2020-09-23 22:05:15 +0100

    added about page

--- src/components/layouts/Spinner.js
+++ src/components/layouts/Spinner.js
@@ -1,10 +1,13 @@
-import React, {Fragment} from 'react'
-import spinner from './spinner.gif';
+import React, { Fragment } from "react";
+import spinner from "./spinner.gif";
 
-export const Spinner = () => {
-    return (
-        <Fragment>
-            <img src={spinner} alt="loading..." style={{width: "200px", margin: "auto", display: "block"}}/>
-        </Fragment>
-    )
-}
+const Spinner = () => (
+  <Fragment>
+    <img
+      src={spinner}
+      alt='loading...'
+      style={{ width: "200px", margin: "auto", display: "block" }}
+    />
+  </Fragment>
+);
+ export default Spinner
